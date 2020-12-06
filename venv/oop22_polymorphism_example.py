b5552dfe411dd5cee80616332cf22e7877e587e0
stepan <shibstep@gmail.com>
2020-12-06 22:28:13 +0300

    aaaaa

--- venv/oop22_polymorphism_example.py
+++ venv/oop22_polymorphism_example.py
@@ -1,5 +1,15 @@
-from oop22_polymorphism import Rectangle
+from oop22_polymorphism import Rectangle, Square, Circle
 
 rectangle_1 = Rectangle(3, 4)
 rectangle_2 = Rectangle(12, 5)
-print(rectangle_1.get_rectangle_area(), rectangle_2.get_rectangle_area)
+
+sqre_1 = Square(5)
+sqre_2 = Square(7)
+
+circle_1 = Circle(5)
+circle_2 = Circle(2)
+
+figures = [rectangle_1, rectangle_2, sqre_1, sqre_2, circle_1, circle_2]
+
+for figure in figures:
+    print(figure, 'area =', figure.get_area())
