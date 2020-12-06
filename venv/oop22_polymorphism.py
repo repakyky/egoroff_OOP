ef634f95dece0006b459edcb8984aa7731fe7750
stepan <shibstep@gmail.com>
2020-12-06 21:59:01 +0300

    aaaaa

---
+++ venv/oop22_polymorphism.py
@@ -0,0 +1,11 @@
+
+# polymorphism
+
+class Rectangle:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_rectangle_area(self):
+        return self.a * self.b
---
+++ venv/oop22_polymorphism_example.py
@@ -0,0 +1,5 @@
+from oop22_polymorphism import Rectangle
+
+rectangle_1 = Rectangle(3, 4)
+rectangle_2 = Rectangle(12, 5)
+print(rectangle_1.get_rectangle_area(), rectangle_2.get_rectangle_area)
